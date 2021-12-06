7a719b3b3ea7c1a7db0736ec5060f15d43fd5f7a
roneytrajano <roneytrajano@gmail.com>
2021-12-06 19:50:35 -0300

    Adicionando scroll horizontal

---
+++ lib/Models/Transaction.dart
@@ -0,0 +1,17 @@
+import 'Contacts.dart';
+
+class Transaction {
+  final double value;
+  final Contact contact;
+
+  Transaction(
+      this.value,
+      this.contact,
+      );
+
+  @override
+  String toString() {
+    return 'Transaction{value: $value, contact: $contact}';
+  }
+
+}
--- lib/Screens/dashboard.dart
+++ lib/Screens/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/Screens/transactions_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -20,36 +21,76 @@ class Dashboard extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Image.asset('images/bytebank_logo.png'),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Material(
-              child: InkWell(
-                onTap: (){
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const ContactsList()));
-                },
-                child: Container(
-                  color: Colors.green,
-                  height: 80,
-                  width: 120,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        Icon(
-                          Icons.people_alt,
-                          color: Colors.white,
-                        ),
-                        Text('Contacts', style: TextStyle(color: Colors.white)),
-                      ],
-                    ),
-                  ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                _FeatureItem(
+                  'Transfer',
+                  Icons.people,
+                    ContactsList()
                 ),
+                _FeatureItem(
+                  'Transaction feed',
+                  Icons.description,
+                    TransactionsList()
+                ),
+                _FeatureItem(
+                    'Exemplo',
+                    Icons.description,
+                    ContactsList()
+                ),
+                _FeatureItem(
+                    'Exemplo',
+                    Icons.description,
+                    ContactsList()
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _FeatureItem extends StatelessWidget {
+  final String _name;
+  final IconData _icon;
+  final _class;
+
+  const _FeatureItem(this._name, this._icon, this._class);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        child: InkWell(
+          onTap: () {
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => _class));
+          },
+          child: Container(
+            color: Colors.green,
+            height: 80,
+            width: 120,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    _icon,
+                    color: Colors.white,
+                  ),
+                  Text(_name, style: const TextStyle(color: Colors.white)),
+                ],
               ),
             ),
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
