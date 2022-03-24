d3050028bfcc115eaa2db0a23dcc921e183826b9
roneytrajano <roneytrajano@gmail.com>
2022-03-24 11:38:25 -0300

    adicionando outras validacoes

--- lib/Screens/transaction_form.dart
+++ lib/Screens/transaction_form.dart
@@ -6,6 +6,7 @@ import 'package:bytebank/Models/Contacts.dart';
 import 'package:bytebank/Models/Transaction.dart';
 import 'package:bytebank/http/webclients/transaction_webclient.dart';
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 class TransactionForm extends StatefulWidget {
   final Contact contact;
@@ -19,6 +20,7 @@ class TransactionForm extends StatefulWidget {
 class _TransactionFormState extends State<TransactionForm> {
   final TextEditingController _valueController = TextEditingController();
   final TransactionWebClient _webClient = TransactionWebClient();
+  final String transactionId = Uuid().v4();
   bool _validate = false;
 
   @override
@@ -29,6 +31,7 @@ class _TransactionFormState extends State<TransactionForm> {
 
   @override
   Widget build(BuildContext context) {
+    print('uuid: $transactionId');
     return Scaffold(
       appBar: AppBar(
         title: const Text('New transaction'),
@@ -87,7 +90,7 @@ class _TransactionFormState extends State<TransactionForm> {
                                     final double? value =
                                     double.tryParse(_valueController.text);
                                     final transactionCreated =
-                                    Transaction(value!, widget.contact);
+                                    Transaction(transactionId, value!, widget.contact);
                                     _save(transactionCreated, password, context);
                                   });
                             });
--- lib/http/webclients/transaction_webclient.dart
+++ lib/http/webclients/transaction_webclient.dart
@@ -17,20 +17,31 @@ class TransactionWebClient{
 
     String jsonInput = jsonEncode(transactionMap);
     final Response response = await client.post(url, headers: {'Content-type': 'application/json', 'password': password,}, body: jsonInput);
+    
+    await Future.delayed(Duration(seconds: 10));
 
 
     if (response.statusCode == 200){
       return Transaction.fromJson(jsonDecode(response.body));
     }
 
-    throw HttpException(_statusCodeResponse[response.statusCode]);
+    throw HttpException(_getMessage(response.statusCode));
 
 
+  }
+
+  String? _getMessage(int statusCode) {
+    if(_statusCodeResponse.containsKey(statusCode)){
+      return _statusCodeResponse[statusCode];
+    }
+
+    return 'Unknown error';
   }
 
   static final Map<int, String> _statusCodeResponse = {
     400 : 'there was an error submitting transaction',
     401 : 'authentication failed',
+    409 : 'transaction already exists',
   };
 }
 
